a7a18ae1113a8c47daf6fd667e54c44aec9e51c1
chenall <chenall.cn@gmail.com>
2009-12-01 05:18:02 +0000

    ﻿同步源码到2009-12-01版

--- stage2/disk_io.c
+++ stage2/disk_io.c
@@ -1118,7 +1118,7 @@ set_device (char *device)
       if (*device != ',' /* && *device != ')' */ )
 	{
 	  char ch = *device;
-	  if (*device == 'f' || *device == 'h' || *device == 'm' || *device == 'r' || *device == 'b' 
+	  if (*device == 'f' || *device == 'h' || *device == 'm' || *device == 'r'
 #ifdef SUPPORT_NETBOOT
 	      || (*device == 'n' && network_ready)
 #endif /* SUPPORT_NETBOOT */
@@ -1148,7 +1148,6 @@ set_device (char *device)
 	      || *device == 'h'
 	      || *device == 'm'
 	      || *device == 'r'
-	      || *device == 'b'
 #ifdef SUPPORT_NETBOOT
 	      || (*device == 'n' && network_ready)
 #endif
@@ -1188,12 +1187,6 @@ set_device (char *device)
 		current_drive = 0xffff;
 	      else if (ch == 'r')
 		current_drive = ram_drive;
-          else if (ch == 'b')
-          {
-         current_partition = install_partition;
-         current_drive = boot_drive;
-         return device + 1;
-         }
 #ifndef GRUB_UTIL
 	      else if (ch == 'h' && (*device == ',' || *device == ')'))
 		{
