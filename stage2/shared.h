c650d5dee4dd2667c58a3e566e663b2f57c8e828
chenall.cn@gmail.com <chenall.cn@gmail.com@ac2b6734-dafe-11de-aa1f-bbd297b2055c>
2012-05-30 16:16:46 +0000

    bug fixed,fsys_iso9660.c
    git-svn-id: https://grub4dos-chenall.googlecode.com/svn/branches/yaya-0.4.6a@292 ac2b6734-dafe-11de-aa1f-bbd297b2055c

--- stage2/shared.h
+++ stage2/shared.h
@@ -868,7 +868,6 @@ extern long realmode_run(long regs_ptr);
 #define ENVI ((char (*)[MAX_ENV_LEN])(BASE_ADDR + MAX_VARS * MAX_VAR_LEN))
 #define _WENV_ 60
 #define WENV_RANDOM (*(unsigned long *)(ENVI[_WENV_]+0x20))
-#define FSYS_FLAGS (*(unsigned long *)(ENVI[_WENV_]+0x24))
 #define QUOTE_CHAR (*(ENVI[_WENV_]+0x30))
 #define WENV_TMP (ENVI[_WENV_]+0x40)
 #define set_envi(var, val)			envi_cmd(var, val, 0)
