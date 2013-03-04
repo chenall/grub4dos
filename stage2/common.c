2f49e9ad000005be68d7e71ce8c33976488ba367
chenall <chenall.cn@gmail.com>
2013-03-04 15:32:04 +0800

    svn r:333 fix r113

--- stage2/common.c
+++ stage2/common.c
@@ -161,6 +161,7 @@ char *err_list[] =
   [ERR_LOAD_SPLASHIMAGE] = "Failed loading splashimage.",
   [ERR_UNIFONT_FORMAT] = "Wrong unifont format.",
   [ERR_UNIFONT_RELOAD] = "Unifont already loaded.",
+  [ERR_DIVISION_BY_ZERO] = "Division by zero",
 
 };
 
