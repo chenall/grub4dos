c292d39959a1fe7a500a2c6875e4251257d79651
chenall <chenall.cn@gmail.com>
2014-08-26 22:52:40 +0800

    allow user to change default variable_base_addr

--- stage2/shared.h
+++ stage2/shared.h
@@ -771,13 +771,14 @@ extern void hexdump(unsigned long,char*,int);
 extern int builtin_cmd (char *cmd, char *arg, int flags);
 extern long realmode_run(long regs_ptr);
 
-
 #define MAX_USER_VARS 60
 #define MAX_VARS 64
 #define MAX_VAR_LEN	8
 #define MAX_ENV_LEN	512
 #define MAX_BUFFER	(MAX_VARS * (MAX_VAR_LEN + MAX_ENV_LEN))
-#define BASE_ADDR 0x45000
+//#define BASE_ADDR 0x45000
+#define VARIABLE_BASE_ADDR (*(unsigned long*)0x307FF4)
+#define BASE_ADDR	VARIABLE_BASE_ADDR
 typedef char VAR_NAME[MAX_VAR_LEN];
 typedef char VAR_VALUE[MAX_ENV_LEN];
 #define VAR ((VAR_NAME *)BASE_ADDR)
