7c4ba84e5fa151473aabae4b129cd82f2176b9e5
yaya <you@example.com>
2019-06-17 10:12:06 +0800

    ﻿. 修正错误。

--- stage2/disk_io.c
+++ stage2/disk_io.c
@@ -500,13 +500,8 @@ devread (unsigned long long sector, unsigned long long byte_offset, unsigned lon
     }
 
   /* Check partition boundaries */
-	if (((unsigned long long)(sector + ((byte_offset + byte_len - 1) >> sector_size_bits)) >= (unsigned long long)part_length))
-	{
-		if (part_start)
-			return !(errnum = ERR_OUTSIDE_PART);
-		else
-			return !(errnum = ERR_READ);
-	}
+  if (((unsigned long long)(sector + ((byte_offset + byte_len - 1) >> sector_size_bits)) >= (unsigned long long)part_length) && part_start)
+      return !(errnum = ERR_OUTSIDE_PART);
 
   /* Get the read to the beginning of a partition. */
   sector += byte_offset >> sector_size_bits;
